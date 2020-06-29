a2ab2751cce0829c37d1cb5d7ddb9267bdab41f4
v13nr <nanang.manajer@gmail.com>
2020-06-29 14:19:48 +0800

    placement & spilOver

--- 02/fungsi.php
+++ 02/fungsi.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 function hitungSisi ($data, $indukId, $sisi, $initLvl = 0, $jumlah = 0){
             
@@ -24,29 +24,23 @@ function hitungSisiOmset ($data, $indukId, $sisi, $initLvl = 0, $jumlah = 0){
     foreach($data as $v) {
         if(0 == $initLvl) {
             if( ($v['induk_id'] == $indukId) && ($v['sisi'] == $sisi) ) {
-               
-                $jumlah += $v["omset"];
-                //if ?
-                $jumlah += hitungSisiOmset($data, $v['id'],  'kiri');
-                $jumlah += hitungSisiOmset($data, $v['id'],  'kanan');
+                $jumlah += $v['omset'];
+                $jumlah += hitungSisiOmset($data, $v['id'], 'kiri');
+                $jumlah += hitungSisiOmset($data, $v['id'], 'kanan');
             }
         }
         else {
             if( ($v['induk_id'] == $indukId) ) 
-                $jumlah += $v["omset"];
+                $jumlah += $v['omset'];
         }
     }
 
     return $jumlah;
 }
 
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=binari', 'root', '');
-    $members = array();
-    foreach($dbh->query('SELECT * from nng_members ORDER BY id ASC') as $row) {
-        array_push($members, $row);
-    }
-    $dbh = null;
+function getBTree($data, $id = 0) {
+    $indukId = 0;
+    $result = array();
 
     $adjust = function($data, $indukId) use (&$adjust) {
         $result = array();
@@ -59,9 +53,8 @@ try {
                 $kanan = hitungSisi($data, $v['id'], 'kanan');
                 $kiri  = hitungSisi($data, $v['id'], 'kiri');
                 
-                $omsetKanan = hitungSisiOmset($data, $v['id'],'kanan');
-                //$omsetKiri = hitungSisiOmset($data, $v['id'], 'kiri',0, $v['omset']); //jika menjumlahkan dengan dirinya sendiri
-                $omsetKiri = hitungSisiOmset($data, $v['id'], 'kiri');
+                $omsetKanan = hitungSisiOmset($data, $v['id'], 0, 'kanan');
+                $omsetKiri = hitungSisiOmset($data, $v['id'], 0, 'kiri');
 
                 $result[] = [
                     'id'=>$v['id'],
@@ -73,7 +66,6 @@ try {
                     'sisi'=>$v['sisi'],
                     'head'=>$v['id']."#".$v['sisi'],
                     'id'=>$v['id'],
-                    'posisi_kecil' => "kiri",
                     'contents'=>'<img src="./Images/' . $v['foto'] .'" class="img-btree" data-id="' . $v['id'] . '" style="width:auto;height:125px"/><br/>
                                  <span>' . $kiri . ' | ' . $kanan . ' </span><br>
                                  <span>' . $omsetKiri . ' | ' . $omsetKanan . ' </span><br>
@@ -87,11 +79,9 @@ try {
         return $result;
     };
 
-    $indukId = 0;
-    $result = array();
-    if(!empty($_GET['id'])) {
-        foreach($members as $v) {
-            if($v['id'] == $_GET['id']) {
+    if(empty($id)) {
+        foreach($data as $v) {
+            if($v['id'] == $id) {
                 $indukId = $v['id'];
 
                 $omsetKanan = $omsetKiri = $kanan = $kiri = 0;
@@ -99,9 +89,8 @@ try {
                 $kanan = hitungSisi($members, $v['id'], 'kanan');
                 $kiri  = hitungSisi($members, $v['id'], 'kiri');
                 
-                $omsetKanan = hitungSisiOmset($members, $v['id'], 'kanan');
-                //$omsetKiri = hitungSisiOmset($members, $v['id'], 'kiri', 0, $v['omset']); //jika menjumlahkan dengan omsetnya sendiri
-                $omsetKiri = hitungSisiOmset($members, $v['id'], 'kiri');
+                $omsetKanan = hitungSisiOmset($members, $v['omset'], 'kanan');
+                $omsetKiri = hitungSisiOmset($members, $v['omset'], 'kiri');
                 
                 $result[0] = [
                     'id'=>$v['id'],
@@ -113,7 +102,6 @@ try {
                     'sisi'=>$v['sisi'],
                     'head'=>$v['nomor']."#".$v['sisi'],
                     'id'=>$v['id'],
-                    'posisi_kecil' => "kiri",
                     'contents'=>'<img src="./Images/' . $v['foto'] .'" class="img-btree" data-id="' . $v['id'] . '" style="width:auto;height:125px"/><br/>
                                     <span>' . $kiri . ' | ' . $kanan . ' </span><br>
                                  <span>' . $omsetKiri . ' | ' . $omsetKanan . ' </span><br>
@@ -122,47 +110,25 @@ try {
                     ];
             }
         }
+
+        $result[0]['children'] = $adjust($data, $indukId);
+    }
+    else {
+        $result = $adjust($data, $indukId);
     }
 
-    if(!empty($_GET['id']))
-        $result[0]['children'] = $adjust($members, $indukId);
-    else
-        $result = $adjust($members, $indukId);
-    
-    // echo '<pre>';
-    // print_r($members);
-    // // print_r($result);
-    // echo '</pre>';
-    echo json_encode($result);
-    //echo "sponsor";
-    
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+    return $result;
 }
-/*
-[{
-                        "head": "A",
-                        "id": "aa",
-                        "contents": "<img src=\"./Images/bjhabibie.jpg\" style=\"width:100%\"/><br/><span>0 | 1</span><br><label>112200054</label><h4>A Contents</h4>",
-                        "children": [
-                            {
-                                "head": "A-1",
-                                "id": "a1",
-                                "contents": "A-1 Contents",
-                                "children": [
-                                    { "head": "A-1-1", "id": "a11", "contents": "A-1-1 Contents" }
-                                ]
-                            },
-                            {
-                                "head": "A-2",
-                                "id": "a2",
-                                "contents": "A-2 Contents",
-                                "children": [
-                                    { "head": "A-2-1", "id": "a21", "contents": "A-2-1 Contents" },
-                                    { "head": "A-2-2", "id": "a22", "contents": "A-2-2 Contents" }
-                                ]
-                            }
-                        ]
-                    }]
-*/                    
+
+function getSpillOverMember($data, $id, $sisi = 'kiri') {
+    $result = [];
+
+    if(!empty($data) && !empty($id) && !empty($sisi)) {
+        foreach($data as $v) {
+
+        }
+
+    }
+
+    return $result;
+}
